f2bd58623a9e2fe583dea1d51fbea3866e781199
Damola12345 <ajiboladamola789@gmail.com>
2022-09-17 13:53:57 +0100

    Terraform IAC AWS

--- week6/terraform/variable.tf
+++ week6/terraform/variable.tf
@@ -1,24 +1,62 @@
 # VPC Variables
 variable "region" {
-  default       = "us-east-1"
-  description   = "AWS Region"
-  type          = string
+  default     = "us-east-1"
+  description = "AWS Region"
+  type        = string
 }
 
 variable "vpc-cidr" {
-  default       = "10.0.0.0/16"
-  description   = "VPC CIDR Block"
-  type          = string
+  default     = "10.0.0.0/16"
+  description = "VPC CIDR Block"
+  type        = string
 }
 
 variable "public-subnet-1-cidr" {
-  default       = "10.0.0.0/24"
-  description   = "Public Subnet 1 CIDR Block"
-  type          = string
+  default     = "10.0.0.0/24"
+  description = "Public Subnet 1 CIDR Block"
+  type        = string
 }
 
 variable "public-subnet-2-cidr" {
-  default       = "10.0.1.0/24"
-  description   = "Public Subnet 2 CIDR Block"
-  type          = string
+  default     = "10.0.1.0/24"
+  description = "Public Subnet 2 CIDR Block"
+  type        = string
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
